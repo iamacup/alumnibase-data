ec79e788b799c668d0a2731b1906ee8c53996212
Megan Field <megan@sliips.com>
2018-04-04 11:19:14 +0100

    splash buttons working for every tab on the sidebar

--- src/content/containers/Pages/Splash/index.js
+++ src/content/containers/Pages/Splash/index.js
@@ -36,27 +36,284 @@ class Page extends React.PureComponent {
         <div className="row">
           <div className="col-md-8 col-md-push-2">
 
-            <div className="row">
-              <div className="col-sm-4">
-                <div className="panel panel-success panel-colorful">
-                  <div className="pad-all media">
-                    <div className="media-left">
-                      <i className="text-3x fas fa-money-bill-alt" />
-                    </div>
-                    <div className="media-body text-center">
-                      <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Salary Data</strong></p>
-                    </div>
-                  </div>
-                  <div className="progress progress-xs progress-success mar-no" />
-                  <div className="pad-all text-sm text-center">
-                    Explore salary for different metrics<br /><br />
-                    <a href="" className="btn btn-primary btn-block">Overview</a>
-                    <a href="" className="btn btn-primary btn-block">Ranges</a>
-                  </div>
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
 
+                  <div className="media-left">
+                    <i className="text-3x fas fa-money-bill-alt" />
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Salary Data</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              {/*  <!--Panel body--> */}
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/salary/Overview" className="btn btn-mint" style={{ marginRight: '8px' }}>Overview</a>
+                  <a href="/analytics/salary/ranges" className="btn btn-mint">Ranges</a>
                 </div>
               </div>
             </div>
+            {/* <!--===================================================--> */}
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+                  <div className="media-left">
+                    <i className="text-3x fas fa-user-md" />
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Jobs and Careers</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/jobs/overview" className="btn btn-mint" style={{ marginRight: '8px' }}>Overview</a>
+                  <a href="/analytics/jobs/employers" className="btn btn-mint" style={{ marginRight: '8px' }}>Employers</a>
+                  <a href="/analytics/jobs/first-year" className="btn btn-mint" style={{ marginRight: '8px' }}>First Year Salary</a>
+                  <a href="/analytics/jobs/first-job" className="btn btn-mint">Time to First Job</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x fas fa-pen-square" />
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Subjects</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/subjects/vfm" className="btn btn-mint" style={{ marginRight: '8px' }}>Value For Money</a>
+                  <a href="/analytics/subjects/3" className="btn btn-mint" style={{ marginRight: '8px' }}>Longterm Outcomes</a>
+                  <a href="/analytics/subjects/first-year" className="btn btn-mint" style={{ marginRight: '8px' }}>First Year Salary</a>
+                  <a href="/analytics/subjects/first-job" className="btn btn-mint">Time to First Job</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x fas fa-chart-line" />
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>STEM</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/stem/overview" className="btn btn-mint" style={{ marginRight: '8px' }}>Overview</a>
+                  <a href="/analytics/stem/destinations" className="btn btn-mint">Destinations and Outcomes</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x fas fa-address-book" />
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>POLAR</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/polar" className="btn btn-mint">Overview</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x fas fa-chevron-double-up" />
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Further Study</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/further-study/overview" className="btn btn-mint">Overview</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x fas fa-eye" />
+
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Respondant Views</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/views/1" className="btn btn-mint" style={{ marginRight: '8px' }}>Views on Education Impact</a>
+                  <a href="/analytics/views/2" className="btn btn-mint">Views on Overall Happiness</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-success panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x fas fa-map" />
+
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Geographic Data</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/destination/1" className="btn btn-mint" style={{ marginRight: '8px' }}>Global</a>
+                  <a href="/analytics/destination/2" className="btn btn-mint">Local</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-8 col-md-push-2">
+            <h3 className="text-main text-normal text-2x mar-no">Regulatory Outputs</h3>
+            <h5 className="text-muted text-normal">General Analytics for your university to tackle the difficult challenges of modern day reporting and improvement.</h5>
+            <hr className="new-section-xs" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-8 col-md-push-2">
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-purple panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x far fa-adjust" />
+
+
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>DLHE-Like</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/dlhe-like/2-3" className="btn btn-pink" style={{ marginRight: '8px' }}>Graduates and What they are Doing</a>
+                  <a href="/analytics/dlhe-like/4" className="btn btn-pink" style={{ marginRight: '8px' }}>Full Time Graduate Destinations</a>
+                  <a href="/analytics/dlhe-like/5" className="btn btn-pink" style={{ marginRight: '8px' }}>Destination, Employment and Earnings</a>
+                  <a href="/analytics/dlhe-like/6" className="btn btn-pink" style={{ marginRight: '8px' }}>Employment Outcomes</a>
+                  <a href="/analytics/dlhe-like/7" className="btn btn-pink" style={{ marginRight: '8px' }}>UK Domicilied Graduates</a>
+                  <a href="/analytics/dlhe-like/8" className="btn btn-pink" style={{ marginRight: '8px' }}>Graduates in Employment</a>
+                  <a href="/analytics/dlhe-like/9" className="btn btn-pink" style={{ marginRight: '8px' }}>Geographical Destinations of Employment</a>
+                  <a href="/analytics/dlhe-like/11" className="btn btn-pink">First Time Graduates in Full Time Work</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
+
+            {/*  <!--===================================================--> */}
+            <div className="panel panel-purple panel-colorful">
+              <div className="panel-heading">
+                <div className="pad-all media">
+
+                  <div className="media-left">
+                    <i className="text-3x far fa-adjust" />
+
+
+                  </div>
+                  <div className="media-body text-left">
+                    <p style={{ fontSize: '16px', marginTop: '8px' }}><strong>Widening Participation</strong></p>
+                  </div>
+                  <div className="media-body text-right">
+                    <p>Explore salary for different metrics</p>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-body">
+                <div className="panel-control">
+                  <a href="/analytics/widening-participation/bme-economic-achievement" className="btn btn-pink" style={{ marginRight: '8px' }}>BME Economic Achievement</a>
+                  <a href="/analytics/widening-participation/improving-technical-pathways" className="btn btn-pink">Improving Technical Pathways of BME and POLAR3</a>
+                </div>
+              </div>
+            </div>
+            {/* <!--===================================================--> */}
 
           </div>
         </div>
