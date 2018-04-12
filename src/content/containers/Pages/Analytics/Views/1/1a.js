fc56f6bbc45990ce6401071b269c2b5beed6a4b3
Megan Field <megan@sliips.com>
2018-04-12 12:19:35 +0100

    data added and slight clean up

--- src/content/containers/Pages/Analytics/Views/1/1a.js
+++ src/content/containers/Pages/Analytics/Views/1/1a.js
@@ -45,69 +45,6 @@ class Page extends React.PureComponent {
     });
   }
 
-  getPercentageBlock(arr, data) {
-    const randombetween = (min, max) => Math.floor(Math.random() * ((max - (min + 1)) + min));
-
-    const generate = (max, thecount) => {
-      const r = [];
-      let currsum = 0;
-      for (let i = 0; i < thecount - 1; i++) {
-        r[i] = randombetween(1, max - (thecount - i - 1) - currsum);
-        currsum += r[i];
-      }
-      r[thecount - 1] = max - currsum;
-      return r;
-    };
-
-    const rands = generate(100, arr.length);
-
-    const rowArr = [];
-
-    for (let a = 0; a < arr.length; a++) {
-      rowArr.push(this.getPercentRow(arr[a], rands[a]));
-    }
-
-    return rowArr;
-  }
-
-  getPercentRow(title, percentage, bottomMargin) {
-    const barStyle = { height: '4px' };
-
-    if (bottomMargin === false) {
-      barStyle.marginBottom = '0';
-    }
-
-    const obj = (
-      <div key={title} className="row">
-        <div className="col-sm-4">
-          <div className="text-left visible-xs-block">
-            <h6 style={{ marginTop: '0' }}>{title}</h6>
-          </div>
-          <div className="text-right hidden-xs">
-            <h6 style={{ marginTop: '0' }}>{title}</h6>
-          </div>
-        </div>
-        <div className="col-sm-8">
-          <h6 style={{ marginTop: '0', marginBottom: '4px' }}>{percentage}%</h6>
-          <div className="progress" style={barStyle}>
-            <div
-              className="progress-bar"
-              role="progressbar"
-              aria-valuenow="70"
-              aria-valuemin="0"
-              aria-valuemax="100"
-              style={{ width: percentage + '%' }}
-            >
-              <span className="sr-only">{percentage}% Complete</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-
-    return obj;
-  }
-
   getOptions1() {
     const axisData = { y: ['1970+', '1980-89', '1990-99', '2000-09', '2010-18'].reverse(), x: '%' };
     const dataSeries = [
@@ -162,7 +99,6 @@ class Page extends React.PureComponent {
 
 
   getOptions3(colours) {
-    console.log(colours)
     const axisData = { y: ['1970+', '1980-89', '1990-99', '2000-09', '2010-18'].reverse(), x: '%' };
     const dataSeries = [
       { name: 'Very Likely', data: [20, 16, 14, 12, 10] },
