fed269fb78ae8d9429dd519f8447f0307b98dfd0
Megan Field <megan@sliips.com>
2018-05-17 10:40:01 +0100

    Sheffield Hallam branding

--- src/content/containers/Fragments/Template/wrapper.js
+++ src/content/containers/Fragments/Template/wrapper.js
@@ -54,7 +54,7 @@ class Wrapper extends React.PureComponent {
           <div id="content-container">
             <div id="page-head">
               <div className="pad-all text-center" style={{ paddingBottom: '0' }}>
-                <h3 style={{ marginTop: '0' }}>University of Sheffield</h3>
+                <h3 style={{ marginTop: '0' }}>Sheffield Hallam University</h3>
                 <p>{this.props.pageData.pageTitle}</p>
               </div>
 
