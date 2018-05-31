fcbe995d70cf837370e832587f46ff57c403b381
Megan Field <megan@sliips.com>
2018-05-31 13:16:08 +0100

    mixed graph on subjects/long-term-outcomes data

--- src/content/containers/Pages/Analytics/Subjects/LongtermOutcomes/index.js
+++ src/content/containers/Pages/Analytics/Subjects/LongtermOutcomes/index.js
@@ -45,29 +45,91 @@ class Page extends React.PureComponent {
     });
   }
 
+  getData(type) {
+    let options = null;
+
+    const line = {
+      lineOptions: { value: false },
+      lineTitles: { x: ['Years After Graduating'], y: ['% of Graduates in Highly Skilled', 'professions (SOC1 - 3)'] },
+      lineData: { 
+        name: ['Social Studies', 'Mathematical Sciences', 'Arts & Humanities'], 
+        age: ['1 Year', '5 Years', '10 Years', '20 Years'],
+        plotted: [[79, 85, 84, 87], [56, 65, 76, 87], [90, 76, 54, 32],
+        ],
+      },
+    }
+
+    const mix = {
+      rawData: [],
+      names: [],
+      titles: [],
+    }
+
+    const percentChange = [];
+
+    if (dNc(this.props.reduxState_fetchDataTransaction.default.payload) && dNc(this.props.reduxState_fetchDataTransaction.default.payload[0])) {
+      Object.keys(this.props.reduxState_fetchDataTransaction.default.payload[0]).forEach(key => {
+        if (type === 'line') {
+        options = drawLineChart(line.lineData, line.lineOptions, line.lineTitles); // top graph data needs to be done!
+        } else if (type === 'salaryChangeTime' && key === type) {
+          this.getAllUniqueNames(this.props.reduxState_fetchDataTransaction.default.payload[0][key])
+            this.props.reduxState_fetchDataTransaction.default.payload[0][key].forEach(elem => {
+              mix.names.push(elem.name)
+              mix.titles = elem.data.map(value => value.subjectType)
+              const data = {data: [], name: elem.name}
+              elem.data.forEach(value => {
+                data.data.push(value.averageSalary)
+              })
+              mix.rawData.push(data);
+            })
+            let changeSalary = mix.rawData[0].data
+
+            mix.rawData[1].data.forEach((elem, i) => {
+              if (elem > changeSalary[i]) {
+                const increase = elem - changeSalary[i];
+                percentChange[i] = ((increase / changeSalary[i]) * 100).toFixed();
+              }
+              if (elem < changeSalary[i]) {
+                const decrease = changeSalary[i] - elem;
+                percentChange[i] = ((decrease / changeSalary[i]) * 100).toFixed();
+              }
+            })
+          options = drawMixGraph(mix.rawData, mix.names, mix.titles, percentChange);
+        }
+      })
+    }
+
+    return options;
+  }
+
+  getAllUniqueNames(dataArr) {
+    const uniqueKeys = [];
+
+    dataArr.forEach((element) => {
+      element.data.forEach((elem) => {
+        if (!uniqueKeys.includes(elem.subjectType)) uniqueKeys.push(elem.subjectType);
+      });
+    });
+
+    dataArr.forEach((element) => {
+      if (element.data.length < uniqueKeys.length) {
+        const keysInBreakdown = element.data.map(elem => elem.subjectType);
+
+        uniqueKeys.forEach((key) => {
+          if (!keysInBreakdown.includes(key)) {
+            if (key === 'Arts, Humanities & Social Sciences') element.data.splice(0, 0, { subjectType: key, averageSalary: 0 });
+            if (key === 'Law') element.data.splice(1, 0, { subjectType: key, averageSalary: 0 });
+            if (key === 'Mathematical Sciences') element.data.splice(2, 0, { subjectType: key, averageSalary: 0 });
+            if (key === 'Medicine & Vetinary Medicine') element.data.splice(3, 0, { subjectType: key, averageSalary: 0 });
+            if (key === 'Other (PG / Research)') element.data.splice(4, 0, { subjectType: key, averageSalary: 0 });
+          }
+        });
+      }
+    });
+    return dataArr;
+  }
+
   getContent() {
-    const lineData = {
-      name: ['Social Studies', 'Mathematical Sciences', 'Arts & Humanities'],
-      age: ['1 Year', '5 Years', '10 Years', '20 Years'],
-      plotted: [
-        [79, 85, 84, 87],
-        [56, 65, 76, 87],
-        [90, 76, 54, 32],
-      ],
-    };
-
-    const lineOptions = {
-      value: false,
-    };
-
-    const lineTitles = { x: ['Years After Graduating'], y: ['% of Graduates in Highly Skilled', 'professions (SOC1 - 3)'] };
-    const rawData = [{ data: [25000, 26000, 27000, 28000], name: '1 Year' }, { data: [50000, 55000, 60000, 70000], name: '10 Years' }];
-    const names = rawData.map(element => element.name);
-    const titles = ['Mathematical Sciences', 'Arts & Humanities', 'Law', 'Engineering & Technology'];
-
-    const lineChartData = drawLineChart(lineData, lineOptions, lineTitles);
-    const mixGraphData = drawMixGraph(rawData, names, titles);
-
     const content = (
       <div id="page-content" key="subjects-longterm">
 
@@ -84,7 +146,7 @@ class Page extends React.PureComponent {
           <div className="col-md-8 col-md-push-2">
 
             <TabbedGraphPanel
-              title={<h4 style={{color: 'red'}}>NEED TO INFER</h4>}
+              title={<p style={{color: 'red'}}>NEED TO INFER</p>}
               globalID="stem-overview-2"
               content={[
                        {
@@ -100,7 +162,7 @@ class Page extends React.PureComponent {
                            width: '100%',
                            height: '400px',
                            data: {
-                             options: lineChartData,
+                             options: this.getData('line'),
                            },
                          },
                        },
@@ -125,7 +187,7 @@ class Page extends React.PureComponent {
                     width: '100%',
                     height: '400px',
                     data: {
-                      options: mixGraphData,
+                      options: this.getData('salaryChangeTime'),
                     },
                   },
                 },
--- src/content/scripts/custom/echarts/drawMixGraph.js
+++ src/content/scripts/custom/echarts/drawMixGraph.js
@@ -1,4 +1,13 @@
-const drawMixGraph = (rawData, names, titles) => {
+const drawMixGraph = (rawData, names, titles, percentChange) => {
+
+  let max = 80000;
+  
+  rawData.forEach(element => {
+    element.data.forEach(elem => {
+      if (elem > max) max = elem;
+    })
+  })
+
   const colors = ['#1c6cab', '#a4c0e5', '#ff7311', '#ffbb7d', '#d02224', '#ff8d8b', '#11293b'];
   names.push('% Change');
   const data = rawData.map((element, i) => (
@@ -18,7 +27,7 @@ const drawMixGraph = (rawData, names, titles) => {
   data.push({
     name: names[names.length - 1],
     type: 'line',
-    data: [100, 112, 122, 150],
+    data: percentChange,
   });
   const textstyle = { fontSize: 12 };
 
@@ -67,7 +76,7 @@ const drawMixGraph = (rawData, names, titles) => {
       },
       {
         type: 'value',
-        max: 80000,
+        max,
         min: 0,
         location: 'center',
         splitLine: {
